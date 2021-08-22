6f12a156e85bda4a2c6262727c561eec8718a11b
Pranesh Palanisamy <pranesh@tocode.tech>
2021-08-22 18:56:08 +0530

    all codes

---
+++ src/components/gallery.jsx
@@ -0,0 +1,207 @@
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Gallery</h2>
+          <p>
+            Unleash your creativety with a visual collaboration platform that enables effective ideation.
+          </p>
+        </div>
+        <div className='row'>
+          <div className='portfolio-items'>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/01-large.jpeg'
+                    title='Work Place'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Work Place</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/01-small.jpeg'
+                      className='img-responsive'
+                      alt='Work Place'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/02-large.jpeg'
+                    title='Development'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Development</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/02-small.jpeg'
+                      className='img-responsive'
+                      alt='Development'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/03-large.jpeg'
+                    title='Discussion'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Discussion</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/03-small.jpeg'
+                      className='img-responsive'
+                      alt='Discussion'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/04-large.jpeg'
+                    title='Motivation'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Motivation</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/04-small.jpeg'
+                      className='img-responsive'
+                      alt='Motivation'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/05-large.jpeg'
+                    title='Writings'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Writings</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/05-small.jpeg'
+                      className='img-responsive'
+                      alt='Writings'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/06-large.jpeg'
+                    title='Laptop'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>MacBook Pro</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/06-small.jpeg'
+                      className='img-responsive'
+                      alt='Laptop'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/07-large.jpeg'
+                    title='Working Together'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Working Together</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/07-small.jpeg'
+                      className='img-responsive'
+                      alt='Working Together'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/08-large.jpeg'
+                    title='Working'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Working</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/08-small.jpeg'
+                      className='img-responsive'
+                      alt='Working'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                  <a
+                    href='img/portfolio/09-large.jpeg'
+                    title='Meeting'
+                    data-lightbox-gallery='gallery1'
+                  >
+                    <div className='hover-text'>
+                      <h4>Meeting</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/09-small.jpeg'
+                      className='img-responsive'
+                      alt='Meeting'
+                    />{' '}
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
